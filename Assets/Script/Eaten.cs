e7c482c0a0da3991f08a98bdc94d2cc8fa0b70b5
Yoko Tsuda <yoko20209@gmail.com>
2021-03-03 10:43:51 +0900

    first commit
    description space

---
+++ Assets/Script/Eaten.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eaten : MonoBehaviour
+{   
+    public GameObject eatenScreen;
+    public bool wasEaten = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        eatenScreen.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(wasEaten){
+            eatenScreen.SetActive(true);
+        }else{
+            eatenScreen.SetActive(false);
+        }
+    }
+}
